f2f24a36002ff7e8ca087b5b3e0acabc4146fafd
RFDaemoniac <robinson.farrar@gmail.com>
2015-01-31 15:14:10 -0500

    Organized Assets, Imported audio

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -42,6 +42,7 @@ public class World : MonoBehaviour {
 	public int deathCount = 0;
 	
 	void Start () {
+		Fabric.EventManager.Instance.PostEvent("Music");
 		Debug.Log("Game Start");
 		respawning = true;
 		respawnTime = Time.time + respawnDelay;
